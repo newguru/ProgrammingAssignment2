1d78439091cb945dd35116f69add8ea467796280
newguru <guru@bvmsllc.com>
2016-02-07 23:30:45 -0600

    PA2 20160207

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,22 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	## Create a matrix
+
+	## Cache mem
+	mem <- NULL
+	set <- function(y) {
+		x <<- y ## assign input to parent environment
+		mem <<- NULL ## initialize mem in parent environment to null
+		}
+
+	get <- function() x ## retrun matrix x
+
+	setinv <- function(inv) mem <<- inv ## set cache to inverse of mem matrix
+
+	getinv <- function() mem ## return cached matrix
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
+
 }
 
 
